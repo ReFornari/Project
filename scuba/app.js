3d45227f323abadb8f2075bf70f27f1114f76a19
renata_fornari <renata_fornari@hotmail.com>
2018-12-05 08:13:17 -0500

    update

--- scuba/app.js
+++ scuba/app.js
@@ -6,31 +6,25 @@ const VIEWS = path.join(__dirname, 'views');
 app.use(express.static("scripts"));
 app.use(express.static("images"));
 
-  
-  //function to set up a simple hello response
 
 app.get('/', function(req, res){
- 
-  res.sendFile('index.html', {root:VIEWS});
-  console.log ("Home Page");
+res.sendFile ('index.jade', {root:VIEWS});
+console.log ("Home Page");
 });
 
 app.get('/courses', function(req, res){
- 
-  res.sendFile('courses.html', {root:VIEWS});
+res.render('/courses.jade', {root:VIEWS});
   console.log ("Courses Page");
 });
 
 app.get('/membership', function(req, res){
- 
-  res.sendFile('membership.html', {root:VIEWS});
-  console.log ("Membership Page");
+res.sendFile('membership.jade', {root:VIEWS});
+console.log ("Membership Page");
 });
 
-app.get('/contact', function(req, res){
- 
-  res.sendFile('contact.html', {root:VIEWS});
-  console.log ("Contact Page");
+app.get('/contact', function(req, res){  
+res.sendFile('contact.jade', {root:VIEWS});
+console.log ("Contact Page");
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0" , function(){
